b0276b490c8d5789171f77c13cac267c3d855eed
Marcello Esposito <esposito.marce@gmail.com>
2019-04-22 22:50:01 +0200

    Fake interface and implementation are available
    Note that the interface is public, whereas the implementation is
    internal. Thus, it is not exposed to other assemblies.

---
+++ DomainModel/FakeInterfaces/IMyFakeInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel.FakeInterfaces
+{
+    public interface IMyFakeInterface
+    {
+        int Sum(int a, int b);
+    }
+}
---
+++ DomainModel/FakeInterfaces/MyFakeImplementation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel.FakeInterfaces
+{
+    class MyFakeImplementation : IMyFakeInterface
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
