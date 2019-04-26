3149e171e70d01ba70fecdb2f02d53cf5b61e1db
Marcello Esposito <esposito.marce@gmail.com>
2019-04-26 19:54:49 +0200

    Composition root configured for CQRS wiring
    Composition root is configured to perform batch registration of the generic
    classes performing the CQRS actions. The batch registration uses the great
    generics support provided by SimpleInjector. The CompositionRoot project
    now references the CQRS project.
    
    The action method can be executed and it provides the expected results.
    A sample URL to be called is the following:
    
    http://baseUrl/api/IntSumCalculator?first=4&Second=14

--- CompositionRoot/RootBindings.cs
+++ CompositionRoot/RootBindings.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
+using CQRS.Commands;
+using CQRS.Queries;
+using DomainModel.CQRS.Queries.GetIntSum;
 using DomainModel.FakeInterfaces;
 using SimpleInjector;
 
@@ -11,6 +15,16 @@ namespace CompositionRoot
         public static void Bind(Container container)
         {
             container.Register<IMyFakeInterface, MyFakeImplementation>(Lifestyle.Scoped);
+
+            var assemblies = new Assembly[]
+            {
+                typeof(GetIntSumQuery).Assembly
+            };
+
+            // The following two lines perform the batch registration by using
+            // the great generics support provided by SimpleInjector.
+            container.Register(typeof(ICommandHandler<>), assemblies);
+            container.Register(typeof(IQueryHandler<,>), assemblies);
         }
     }
 }
