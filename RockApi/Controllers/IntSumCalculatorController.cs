2c75bd2f6d64e7be0bf379b1e915e06dc32cf27d
Marcello Esposito <esposito.marce@gmail.com>
2019-04-27 16:39:19 +0200

    WRONG way of logging enabled
    Systematic query and command logging enables auditing and this is
    highly desirable.
    
    In this version GetIntSum query is logged. Two log lines are generated
    for each execution.
    
    The first:
    
    2019-04-27 16:38:17.697 +02:00 [Information] Action starting "DomainModel.CQRS.Queries.GetIntSum.GetIntSumQuery": "{\"First\":4,\"Second\":13}"
    
    This line is generated BEFORE the action is executed and it reports the
    execution time, the class of the query being executed, the DTO value
    (who executes the query and the id of the executing thread are missing
    so far, but we will fix this later).
    
    The second:
    
    2019-04-27 16:38:17.702 +02:00 [Information] Action executed (0 ms)
    
    This line is generated AFTER the action is executed and it just reports
    the execution time.
    
    Nevertheless, this approach is wrong. Logging is a cross-cutting concern,
    but in this approach it is not treated as such: each query must add
    this (ugly) logging code and, in case we forget to do it, the log would
    be missing with no evidence.
    
    So, the generated log is useful and correct, but the approach is wrong.
    In the following commits we will revert to the previous version and we
    will follow a more correct, aspect-oriented, approach.

--- RockApi/Controllers/IntSumCalculatorController.cs
+++ RockApi/Controllers/IntSumCalculatorController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using CQRS.Queries;
 using DomainModel.CQRS.Queries.GetIntSum;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace RockApi.Controllers
 {
@@ -24,7 +26,19 @@ namespace RockApi.Controllers
         [HttpGet]
         public ActionResult<int> Get([FromQuery] GetIntSumQuery query)
         {
-            return this.handler.Handle(query);
+            var jsonQuery = Newtonsoft.Json.JsonConvert.SerializeObject(query);
+            var queryClass = query.GetType().ToString();
+            Log.Information("Action starting {queryClass}: {jsonQuery}", queryClass, jsonQuery);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var result = this.handler.Handle(query);
+            stopwatch.Stop();
+
+            var elapsed = stopwatch.ElapsedMilliseconds;
+            Log.Information("Action executed ({elapsed} ms)", elapsed);
+
+            return result;
         }
     }
 }
