0d1fba67b2cfc7a99abeabae7f99e5d842495fea
Marcello Esposito <esposito.marce@gmail.com>
2019-05-01 18:08:07 +0200

    Authorization infrastructure available for commands
    Authorization on action execution is another common cross-cutting
    concern. For the sake of integrating
    authorization in the same way we did for validation, a set of classes is
    available. They in all respects resemble those already available for validation.

---
+++ CQRS/Commands/Authorizers/AuthorizingCommandHandlerDecorator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CQRS.Authorization;
+
+namespace CQRS.Commands.Authorizers
+{
+    public class AuthorizingCommandHandlerDecorator<TCommand> : ICommandHandler<TCommand>
+    {
+        private readonly ICommandHandler<TCommand> decoratee;
+        private readonly IEnumerable<ICommandAuthorizer<TCommand>> authorizers;
+
+        public AuthorizingCommandHandlerDecorator(
+            IEnumerable<ICommandAuthorizer<TCommand>> authorizers,
+            ICommandHandler<TCommand> decoratee)
+        {
+            this.authorizers = authorizers;
+            this.decoratee = decoratee;
+        }
+
+        public void Handle(TCommand command)
+        {
+            var authorizationResults = (
+                from authorizer in this.authorizers
+                from result in authorizer.Authorize(command)
+                select result)
+                .ToArray();
+
+            if (authorizationResults.Any())
+            {
+                throw new AuthorizationException(authorizationResults);
+            }
+
+            this.decoratee.Handle(command);
+        }
+    }
+}
